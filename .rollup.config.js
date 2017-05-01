4fe0c562fbce07772b1206260e6298ecceda3b3a
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-05-01 21:43:33 +0200

    Remove uglify

--- .rollup.config.js
+++ .rollup.config.js
@@ -1,13 +1,8 @@
 import babel from 'rollup-plugin-babel'
-import uglify from 'rollup-plugin-uglify'
 import resolve from 'rollup-plugin-node-resolve'
 
 const isRelease = process.env[ 'BUILD_MODE' ] === 'release'
 const plugins = [ babel(), resolve({ jsnext: true, modulesOnly: true }) ]
-
-if (isRelease) {
-  plugins.push(uglify())
-}
 
 export default {
   plugins: plugins,
